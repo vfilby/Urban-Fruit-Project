0b86174e23dc40d2c1fba8a7328bb57711b70149
Vincent Filby <vfilby@gmail.com>
2012-04-01 15:26:50 -0400

    Updating omniauth for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  provider :openid, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  
 end
