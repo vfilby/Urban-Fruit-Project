796de33a9e95ecbb2f18a072c47c873a4833c2db
Vincent Filby <vfilby@gmail.com>
2012-04-06 00:05:27 -0400

    Changing to the new aws-sdk gem Moved indextank config to ENV

--- config/initializers/tanker.rb
+++ config/initializers/tanker.rb
@@ -1,3 +1,1 @@
-#Urbanfruitproject::Application.config.index_tank_url = 'http://:Ugu7pIJIi6NNWm@8t1ko.api.indextank.com'
-#Urbanfruitproject::Application.config.index_tank_url = 'http://:KBHu89cUFtxeWxN@urbanfruitproject.api.houndsleuth.com'
-Urbanfruitproject::Application.config.index_tank_url = 'http://:tajytunypape@behage.api.indexden.com'
+Urbanfruitproject::Application.config.index_tank_url = ENV['INDEXTANK_URL']
