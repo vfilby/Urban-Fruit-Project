b38e1e80ab20bc052ddbe488a380c1eeebf1377a
Vincent Filby <vfilby@gmail.com>
2012-05-01 21:34:07 -0400

    Moved s3 to a project directory

--- sync-to-dev.sh
+++ sync-to-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 heroku db:pull --confirm=urbanfruitproject
-~/Downloads/s3cmd-1.1.0-beta3/s3cmd sync --delete-removed --acl-public s3://urbanfruitproject s3://urbanfruitproject-dev
+./script/s3cmd-1.1.0-beta3/s3cmd sync --delete-removed --acl-public s3://urbanfruitproject s3://urbanfruitproject-dev
 
 source .powrc
 rake tanker:reindex
