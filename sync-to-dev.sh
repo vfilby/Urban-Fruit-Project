000fff71fab1d24e36d8e5fbcac5b75e3355bd79
Vincent Filby <vfilby@gmail.com>
2012-05-23 10:16:10 -0400

    Removed whitespace

--- sync-to-dev.sh
+++ sync-to-dev.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rake db:drop
-
 heroku db:pull --confirm=urbanfruitproject
 rake db:migrate
 ./script/s3cmd-1.1.0-beta3/s3cmd sync --delete-removed --acl-public s3://urbanfruitproject s3://urbanfruitproject-dev
