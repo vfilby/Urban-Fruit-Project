05c41e33ebd475d53514183867ccdebe16ad0bfd
Vincent Filby <vfilby@gmail.com>
2012-05-31 18:44:43 -0400

    Adding omniauth integration tests

---
+++ test/integration/omni_auth_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class OmniAuthTest < ActionController::TestCase
+  
+  setup do
+     OmniAuth.config.test_mode = true
+  end
+  
+  test "user info should contain email" do
+    get '/auth/google/callback'
+    debugger
+    
+  end
+
+end
